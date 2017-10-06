19c9c036526e741bebcc2f524b8562188a73c828
Karli Raudsepp <karliraudsepp@hazarada.htk.tlu.ee>
2017-10-06 12:02:12 +0300

    temporary fix for invasive geogebra css

--- geogebra_webloader.js
+++ geogebra_webloader.js
@@ -4,4 +4,18 @@
 	var script = document.createElement("script");
 	script.src = "https://cdn.geogebra.org/apps/deployggb.js";
 	document.head.appendChild(script);
+
+	var fix_func = function() {
+		var offender = document.head.querySelector("style.ggw_resource");
+		if (offender == null) {
+			setTimeout(fix_func, 100);
+			return;
+		}
+		offender.innerHTML = offender.innerHTML.replace("}.toolbar{", "}.geogebra_wrapper .toolbar{");
+		
+	};
+	fix_func();
+
+
+
 })();
